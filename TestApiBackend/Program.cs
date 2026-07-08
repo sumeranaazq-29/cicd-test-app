f7f0dc825ac218ab39070722d78df2c987173a45
sumeranaazq-29 <sumeranaazq@gmail.com>
2026-07-08 10:45:15 +0000

    Initial commit: test .NET backend + React frontend

---
+++ TestApiBackend/Program.cs
@@ -0,0 +1,18 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+    });
+});
+
+var app = builder.Build();
+
+app.UseCors("AllowAll");
+
+app.MapGet("/api/test", () => Results.Ok(new { message = "Hello from .NET backend! CI/CD pipeline test app is working.", timestamp = DateTime.UtcNow }));
+
+app.Urls.Add("http://0.0.0.0:5000");
+app.Run();
