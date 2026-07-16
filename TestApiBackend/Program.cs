f6f82ec28685b9b93ed9eb9dce45e07912875b3c
sumeranaazq-29 <sumeranaazq@gmail.com>
2026-07-16 11:57:16 +0000

    Test change to verify backup-before-deploy flow

--- TestApiBackend/Program.cs
+++ TestApiBackend/Program.cs
@@ -12,7 +12,7 @@ var app = builder.Build();
 
 app.UseCors("AllowAll");
 
-app.MapGet("/api/test", () => Results.Ok(new { message = "Hello from .NET backend! CI/CD pipeline test app is working.", timestamp = DateTime.UtcNow }));
+app.MapGet("/api/test", () => Results.Ok(new { message = "Hello from .NET backend! (v2 - post backup test) CI/CD pipeline test app is working.", timestamp = DateTime.UtcNow }));
 
 app.Urls.Add("http://0.0.0.0:5000");
 app.Run();
